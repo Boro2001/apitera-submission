4e662114fd5ace49a8b52bd30cee232888aaf3e3
plgborowiczm <mikolajborowicz7@gmail.com>
2023-08-25 18:27:23 +0200

    # updates

--- src/main/java/com/atipera/githubrepotioryfetchingapi/exception/BadUserException.java
+++
@@ -1,7 +0,0 @@
-package com.atipera.githubrepotioryfetchingapi.exception;
-
-public class BadUserException extends RuntimeException {
-    public BadUserException(String format) {
-        super(format);
-    }
-}
